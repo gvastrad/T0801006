7f1a385b868f5d781706a657fe39918983591aec
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2024-12-05 20:28:01 +0530

    first commit

---
+++ T0801006_Core_Java/src/com/tnsif/inheritance/miltilevel/Word.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.tnsif.inheritance.miltilevel;
+
+/**
+ * @author Sagar
+ *
+ */
+public class Word extends WordPad{
+
+	 private boolean spellCheckEnabled;
+	 
+	 public Word() {
+	        super();
+	        this.spellCheckEnabled = true;
+	    }
+	 
+	  public void spellCheck() {
+	        if (spellCheckEnabled) {
+	            System.out.println("Running spell check...");
+	            // Simulate spell check logic here
+	        } else {
+	            System.out.println("Spell check is disabled.");
+	        }
+	    }
+	  
+	  public void enableSpellCheck(boolean enable) {
+	        spellCheckEnabled = enable;
+	    }
+}
